7c6eeb8667f1fba8ae9c1323f4ad770d3015561a
ImranRH321 <imranrh247@gmail.com>
2023-04-14 15:09:07 +0600

    n-foun-data

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 document.getElementById("search_button").addEventListener("click", () => {
   const inputSearch = document.getElementById("inputSearchText");
   const userSearchPhone = inputSearch.value;
-  // loadData(userSearchPhone);
-  loadData("oppo");
+  loadData(userSearchPhone);
 });
 
 async function loadData(searchText) {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  console.log(url);
   const response = await fetch(url);
   const loadPhones = await response.json();
   const phones = loadPhones.data;
@@ -17,6 +15,16 @@ async function loadData(searchText) {
 // ** single item display  **
 const displayShowingPhones = (phones) => {
   const parenContainerDiv = document.getElementById("parenContainerDiv");
+  parenContainerDiv.innerHTML = "";
+  const noFound = document.getElementById("no-found");
+  if (phones.length === 0) {
+    noFound.classList.remove("d-none");
+  } else {
+    noFound.classList.add("d-none");
+  }
+
+  phones = phones.slice(0, 5);
+
   phones.forEach((phone) => {
     const div = document.createElement("div");
     div.classList.add("col");
@@ -41,23 +49,21 @@ const displayShowingPhones = (phones) => {
     parenContainerDiv.appendChild(div);
   });
 };
+
 // ** load single details phone **
 const loadDetailsPhones = async (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-
   const res = await fetch(url);
   const phones = await res.json();
-
   if (phones.status) {
     displayDetailsModalShowPhones(phones.data);
   }
 };
 
-//
+// ** single  details phone ** //
 const displayDetailsModalShowPhones = (phones) => {
-  const { image, brand, mainFeatures, name, releaseDate, others, slug } =
-    phones;
-  const { Bluetooth, GPS, NFC, Radio, USB} = others;
+  const { image, brand, name, releaseDate, others } = phones;
+  const { Bluetooth, GPS, NFC, Radio, USB } = others;
 
   document.querySelector(".modal-title").innerHTML = brand;
   document.querySelector(".modal-body").innerHTML = `
@@ -74,6 +80,12 @@ const displayDetailsModalShowPhones = (phones) => {
    <p>NFC: ${NFC}</p>
    </div>
   `;
-
-  console.log(others, mainFeatures);
 };
+
+
+/* 
+1.load phone
+1.load details phone.
+ 1.phone search no result found then error data is no found 
+ 2.phone search data found then no found hide  
+*/
